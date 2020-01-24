0e352db86d78a5c824ae374233c32bf7ce4d709b
Charan Teja Reddy <charante@codeaurora.org>
2020-01-24 13:58:11 +0530

    ion: don't wakeup refill threads for order-0 requests
    We check the pool->count against the ION_POOL_LOW_MARK before deciding
    to wakeup the refill threads. But as we never refill the order-0 pool,
    we will keep waking up these refill threads only they goto sleep again
    without doing any work. The overhead of waking up of these threads
    is quite high. Fix it.
    
    Change-Id: I0d10b96a46de24b45822ebeda1f20b75e43c2a5d
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -87,7 +87,7 @@ static struct page *alloc_buffer_page(struct ion_system_heap *heap,
 
 	page = ion_page_pool_alloc(pool, from_pool);
 
-	if (pool_auto_refill_en &&
+	if (pool_auto_refill_en && pool->order &&
 	    pool_count_below_lowmark(pool) && vmid <= 0) {
 		wake_up_process(heap->kworker[cached]);
 	}
