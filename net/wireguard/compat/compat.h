ae4ed3d05fe3f6ae54f1d2eb5b01ff40eea04bba
madmax7896 <thegreatsagar123@gmail.com>
2021-01-24 20:06:28 +0000

    net: Fixup for a redefination
    This is already defined in include/linux/skbuff.h and thus gives a redefination error on compilation of wireguard.

--- net/wireguard/compat/compat.h
+++ net/wireguard/compat/compat.h
@@ -827,10 +827,6 @@ static __always_inline void old_rcu_barrier(void)
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 10) && !defined(ISRHEL8)
-static inline void skb_mark_not_on_list(struct sk_buff *skb)
-{
-	skb->next = NULL;
-}
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0) && !defined(ISRHEL8)
