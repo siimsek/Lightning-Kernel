b8b2cc2aa37be5bda671c97efd23d0ac527b840f
madmax7896 <thegreatsagar123@gmail.com>
2021-02-24 16:27:59 +0000

    build: Clone my fork of proton clang

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ ZIPNAME="SurgeX-ginkgo-$(date '+%Y%m%d-%H%M').zip"
 
 if ! [ -d "$HOME/proton" ]; then
 echo "Proton clang not found! Cloning..."
-if ! git clone -q https://github.com/kdrag0n/proton-clang --depth=1 --single-branch ~/proton; then
+if ! git clone -q https://github.com/madmax7896/proton-clang --depth=1 --single-branch ~/proton; then
 echo "Cloning failed! Aborting..."
 exit 1
 fi
