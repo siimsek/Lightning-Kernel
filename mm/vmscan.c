cdf825226a710247cf0afde78429546e0dbacfe8
Zhangyao,Ye <zhangyao@codeaurora.org>
2021-04-28 21:33:47 +0800

    Disable vmscan warning print
    When free memory drops to a  threshold, system high frequency print
    call stack message, too much print log affect system normal run.
    
    Change-Id: I9b1cb84537486e2979cb93ac9a248bec85453d9c
    Signed-off-by: wya <wya@codeaurora.org>
    Signed-off-by: Muhammed Ali Simsek <malisimsek17@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1716,7 +1716,7 @@ int isolate_lru_page(struct page *page)
 	int ret = -EBUSY;
 
 	VM_BUG_ON_PAGE(!page_count(page), page);
-	WARN_RATELIMIT(PageTail(page), "trying to isolate tail page");
+	/* WARN_RATELIMIT(PageTail(page), "trying to isolate tail page"); */
 
 	if (PageLRU(page)) {
 		pg_data_t *pgdat = page_pgdat(page);
