9562cecb2f06a89510dd99a38a4e913a9fd9eea2
siimsek <malisimsek17@gmail.com>
2022-12-16 14:02:07 -0500

    Moved to Neutron 16.0.0 Clang

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="Lightning.Kernel-ginkgo_A11-A13_$(TZ=Europe/Istanbul date +"%Y%m%d-%H%M")-AC.zip"
-TC_DIR="$HOME/tc/aosp-clang"
+TC_DIR="$HOME/tc/neutron"
 GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
 GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
 AK3_DIR="$HOME/android/AnyKernel3"
@@ -12,14 +12,6 @@ export PATH="$TC_DIR/bin:$PATH"
 export KBUILD_BUILD_USER="siimsek"
 export KBUILD_BUILD_HOST="linux"
 export KBUILD_BUILD_VERSION="1"
-
-if ! [ -d "${TC_DIR}" ]; then
-echo "Clang not found! Cloning to ${TC_DIR}..."
-if ! git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r450784.git ${TC_DIR}; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
-fi
 
 if ! [ -d "${GCC_64_DIR}" ]; then
 echo "gcc not found! Cloning to ${GCC_64_DIR}..."
