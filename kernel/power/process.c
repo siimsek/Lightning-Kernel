17b8374ca611bb5784de8a69dead1eff921aa8ef
Muhammed Ali <62520983+siimsek@users.noreply.github.com>
2023-01-10 23:44:04 +0300

    power: Nuke some logspams
    [45312.783042] (elapsed 0.010 seconds) done.
    [45312.792378] (elapsed 0.009 seconds) done.
    [45313.059197] done.
    [45313.189011] (elapsed 0.010 seconds) done.
    [45313.197054] (elapsed 0.008 seconds) done.
    [45313.472763] done.
    [45313.598602] (elapsed 0.009 seconds) done.
    [45313.606451] (elapsed 0.007 seconds) done.
    [45313.874532] done.
    [45314.003366] (elapsed 0.009 seconds) done.
    [45314.011173] (elapsed 0.007 seconds) done.
    [45314.273979] done.
    [45314.403704] (elapsed 0.011 seconds) done.
    [45314.413394] (elapsed 0.009 seconds) done.
    [45314.685550] done.
    [45314.814340] (elapsed 0.011 seconds) done.
    [45314.824350] (elapsed 0.009 seconds) done.
    [45315.102244] done.
    [45315.231443] (elapsed 0.011 seconds) done.
    [45315.241523] (elapsed 0.010 seconds) done.
    [45315.517514] done.
    [45315.646542] (elapsed 0.010 seconds) done.
    [45315.656076] (elapsed 0.009 seconds) done.
    [45315.939318] done.
    
    Signed-off-by: siimsek <malisimsek17@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -109,8 +109,6 @@ static int try_to_freeze_tasks(bool user_only)
 		}
 		read_unlock(&tasklist_lock);
 	} else {
-		pr_cont("(elapsed %d.%03d seconds) ", elapsed_msecs / 1000,
-			elapsed_msecs % 1000);
 	}
 
 	return todo || wakeup ? -EBUSY : 0;
@@ -143,9 +141,7 @@ int freeze_processes(void)
 	error = try_to_freeze_tasks(true);
 	if (!error) {
 		__usermodehelper_set_disable_depth(UMH_DISABLED);
-		pr_cont("done.");
 	}
-	pr_cont("\n");
 	BUG_ON(in_atomic());
 
 #ifndef CONFIG_HAVE_LOW_MEMORY_KILLER
@@ -181,9 +177,6 @@ int freeze_kernel_threads(void)
 	pm_nosig_freezing = true;
 	error = try_to_freeze_tasks(false);
 	if (!error)
-		pr_cont("done.");
-
-	pr_cont("\n");
 	BUG_ON(in_atomic());
 
 	if (error)
@@ -227,7 +220,6 @@ void thaw_processes(void)
 	usermodehelper_enable();
 
 	schedule();
-	pr_cont("done.\n");
 	trace_suspend_resume(TPS("thaw_processes"), 0, false);
 }
 
@@ -248,5 +240,4 @@ void thaw_kernel_threads(void)
 	read_unlock(&tasklist_lock);
 
 	schedule();
-	pr_cont("done.\n");
 }
