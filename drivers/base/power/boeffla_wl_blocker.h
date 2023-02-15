91ba73893134a25e8f67202f6bace521f3ba46ea
Muhammed Ali Simsek <malisimsek17@gmail.com>
2023-02-15 18:22:44 +0300

    boeffla_wl_blocker: Block DIAG_WS
    Signed-off-by: Muhammed Ali Simsek <malisimsek17@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;wlan_rx_wake;wlan_ctrl_wake;wlan_wake;qbt_wake_source;IPA_CLIENT_APPS_LAN_CONS;IPA_CLIENT_APPS_WAN_CONS;rmnet_ipa%d;CHG_PLCY_MAIN_WL"
+#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;wlan_rx_wake;wlan_ctrl_wake;wlan_wake;qbt_wake_source;IPA_CLIENT_APPS_LAN_CONS;IPA_CLIENT_APPS_WAN_CONS;rmnet_ipa%d;CHG_PLCY_MAIN_WL;DIAG_WS"
 
 #define LENGTH_LIST_WL				300
 #define LENGTH_LIST_WL_DEFAULT		239
