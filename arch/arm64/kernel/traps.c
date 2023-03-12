28ed65f41944512e3ac959f61c830c175bf9fc40
Muhammed Ali Simsek <malisimsek17@gmail.com>
2023-03-12 09:40:12 +0300

    Revert "FROMLIST: arm64: kernel: Implement fast refcount checking"
    This reverts commit f38adcc1accd1cd56a9da9be9e53e32aad5d6226.
    
    Signed-off-by: Muhammed Ali Simsek <malisimsek17@gmail.com>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -976,7 +976,6 @@ static struct break_hook refcount_break_hook = {
 void __init trap_init(void)
 {
 	register_kernel_break_hook(&bug_break_hook);
-	register_kernel_break_hook(&refcount_break_hook);
 #ifdef CONFIG_KASAN_SW_TAGS
 	register_kernel_break_hook(&kasan_break_hook);
 #endif
