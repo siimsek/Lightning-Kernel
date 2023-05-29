b7c50318ca736b5caca126398e3a18c4edc5b160
STFU <Dedekiswara16@gmail.com>
2023-05-29 21:36:30 +0000

    Merge branch 'aosp-new/upstream-linux-4.14.y' into KSU

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="Lightning.Kernel_$(TZ=Europe/Istanbul date +"%Y%m%d-%H%M").zip"
-TC_DIR="$HOME/tc/r487747"
-GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
-GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
-AK3_DIR="$HOME/android/AnyKernel3"
+ZIPNAME="SuperV01D.Kernel_$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
+TC_DIR="/workspace/V01D/tc/proton-clang"
+GCC_64_DIR="/workspace/V01D/tc/aarch64-linux-android-4.9"
+GCC_32_DIR="/workspace/V01D/tc/arm-linux-androideabi-4.9"
+AK3_DIR="/workspace/V01D/android/AnyKernel3"
 DEFCONFIG="vendor/ginkgo-perf_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
-export KBUILD_BUILD_USER="siimsek"
-export KBUILD_BUILD_HOST="linux"
-export KBUILD_BUILD_VERSION="1"
+export KBUILD_BUILD_USER="V01D"
+export KBUILD_BUILD_HOST="Gitpod"
+
 
 if ! [ -d "${TC_DIR}" ]; then
 echo "Clang not found! Cloning to ${TC_DIR}..."
-if ! git clone --depth=1 https://gitlab.com/moehacker/clang-r487747.git ${TC_DIR}; then
+if ! git clone --depth=1 https://github.com/kdrag0n/proton-clang ${TC_DIR}; then
 echo "Cloning failed! Aborting..."
 exit 1
 fi
@@ -57,7 +57,7 @@ if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.i
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/siimsek/AnyKernel3; then
+elif ! git clone -q https://github.com/V01D-co/AnyKernel3; then
 echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
 exit 1
 fi
