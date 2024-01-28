c9e2e1c25ac2473a86d212480e5acb8826921d43
Muhammed Ali Simsek <malisimsek17@gmail.com>
2024-01-28 13:17:15 +0300

    Merge tag 'v4.14.334' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux
    This is the 4.14.334 stable release
    * tag 'v4.14.334' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux: (1711 commits)
      Linux 4.14.334
      powerpc/ftrace: Fix stack teardown in ftrace_no_trace
      powerpc/ftrace: Create a dummy stackframe to fix stack unwind
      ring-buffer: Fix memory leak of free page
      team: Fix use-after-free when an option instance allocation fails
      ext4: prevent the normalized size from exceeding EXT_MAX_BLOCKS
      HID: hid-asus: add const to read-only outgoing usb buffer
      net: usb: qmi_wwan: claim interface 4 for ZTE MF290
      asm-generic: qspinlock: fix queued_spin_value_unlocked() implementation
      HID: multitouch: Add quirk for HONOR GLO-GXXX touchpad
      HID: hid-asus: reset the backlight brightness level on resume
      platform/x86: intel_telemetry: Fix kernel doc descriptions
      bcache: add code comments for bch_btree_node_get() and __bch_btree_node_alloc()
      blk-throttle: fix lockdep warning of "cgroup_mutex or RCU read lock required!"
      cred: switch to using atomic_long_t
      appletalk: Fix Use-After-Free in atalk_ioctl
      vsock/virtio: Fix unsigned integer wrap around in virtio_transport_has_space()
      sign-file: Fix incorrect return values check
      net: Remove acked SYN flag from packet in the transmit queue correctly
      qed: Fix a potential use-after-free in qed_cxt_tables_alloc
      ...

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -1320,3 +1320,9 @@ void binder_alloc_shrinker_exit(void)
 	unregister_shrinker(&binder_shrinker);
 	list_lru_destroy(&binder_alloc_lru);
 }
+
+void binder_alloc_shrinker_exit(void)
+{
+	unregister_shrinker(&binder_shrinker);
+	list_lru_destroy(&binder_alloc_lru);
+}
